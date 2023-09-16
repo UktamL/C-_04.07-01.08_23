1209cf7712f546173aa59e1cab8118d896f6fba8
Yktam <lygkob@yandex.ru>
2023-09-16 13:46:14 +0300

    add Task14/

---
+++ Task14/Program.cs
@@ -0,0 +1,17 @@
+﻿// 14. Напишите прорамму, которая принимает на вход
+// число и проверяет, кратно ли оно одновременно 7 и 23.
+// 14 -> no
+// 46 -> no
+// 161 -> yes
+
+Console.WriteLine("Введите число :");
+int number = Convert.ToInt32(Console.ReadLine());
+
+bool twoBool = TwoBool(number);
+Console.WriteLine(twoBool ? "yes" : "no");
+
+
+bool TwoBool(int num1 ){
+    return num1 % 7 == 0 && num1 % 23 == 0 ;
+      
+}
